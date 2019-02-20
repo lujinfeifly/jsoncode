955250b5da9070a0814fc17f6dcda13327fbdef9
lujinfei <lujinfeifly@aliyun.com>
2019-02-20 12:58:18 +0800

    list的逻辑加入

--- src/main/java/cn/miludeer/jsoncode/JsonCode.java
+++ src/main/java/cn/miludeer/jsoncode/JsonCode.java
@@ -15,9 +15,9 @@ public class JsonCode {
 
     /**
      * 从某个字符串获取制定目标的值。
-     * @param jsonStr
-     * @param expression
-     * @return
+     * @param jsonStr 目标json字符串
+     * @param expression 指定标识
+     * @return 以字符串返回的值，如果类型为list直接返回json的列表字符串
      */
     public static String getValue(String jsonStr, String expression) {
         if(jsonStr == null || jsonStr.length() == 0 ||
@@ -45,6 +45,12 @@ public class JsonCode {
         return jsonStr.substring(a,b);
     }
 
+    /**
+     * 从json字符串中火气指定标识的list的列表，返回为String[],如果对象不为数组结构的，则返回null
+     * @param jsonStr 目标json字符串
+     * @param expression 指定标示
+     * @return 返回的字符串表示的列表
+     */
     public static String[] getValueList(String jsonStr, String expression) {
         String dis = getValue(jsonStr, expression);
         if(dis == null || dis.length()<1 || dis.charAt(0) != '[') {
