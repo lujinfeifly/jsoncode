f1fe93b95a0e12b83521bb8aa3f9a90e91e87392
lujinfei <lujinfeifly@aliyun.com>
2019-02-20 13:07:26 +0800

    test

--- src/test/java/cn/miludeer/jsoncode/Test/JsonCodeTest.java
+++ src/test/java/cn/miludeer/jsoncode/Test/JsonCodeTest.java
@@ -5,24 +5,13 @@ import org.junit.Test;
 
 /**
  * program: jsoncode
- * <p>
  * description: ${description}
- * <p>
  * author: lujinfei
- * <p>
  * create: 2019-02-17 10:49
  **/
 public class JsonCodeTest {
 
-    @Test
-    public void testList() {
 
-        String json = "{\"ss\":{\"sss\":\"vvvvv\",\"fg\":{\"f\":\"ererer\",\"list\":[1,eeee,{\"\\[\"}]}}}";
-
-        String code = JsonCode.getValue(json,"*.ss.fg.list");
-
-        System.out.println(code);
-    }
 
     @Test
     public void testkey() {
@@ -30,6 +19,16 @@ public class JsonCodeTest {
         String json = "{\"ss\":{\"sss\":\"vvvvv\",\"fg\":{\"f\":\"ererer\",\"list\":[1,eeee,{\"\\[\"}]}}}";
 
         String code = JsonCode.getValue(json,"*.ss.fg.f");
+
+        System.out.println(code);
+    }
+
+    @Test
+    public void testList() {
+
+        String json = "{\"ss\":{\"sss\":\"vvvvv\",\"fg\":{\"f\":\"ererer\",\"list\":[1,eeee,{\"\\[\"}]}}}";
+
+        String code = JsonCode.getValue(json,"*.ss.fg.list");
 
         System.out.println(code);
     }
